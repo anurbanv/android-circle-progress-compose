2a92c27e63dfffcfd415b711a9ab187bbe7539c0
Andrius Urbanavičius <andrius.urbanavicius@surfsharkteam.com>
2025-03-31 16:33:47 +0700

    feat: circle background component

---
+++ app/src/main/java/com/example/circleprogressanimation/components/CircleRing.kt
@@ -0,0 +1,31 @@
+package com.example.circleprogressanimation.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.circleprogressanimation.ui.theme.CoolGray
+
+@Composable
+fun CircleRing(
+    radius: Float = 100f,
+    thickness: Float = 20f,
+) {
+    Canvas(modifier = Modifier.size((radius * 2 + thickness).dp)) {
+        drawCircle(
+            color = CoolGray,
+            radius = radius,
+            style = Stroke(width = thickness)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CircleRingPreview() {
+    Box { CircleRing() }
+}
